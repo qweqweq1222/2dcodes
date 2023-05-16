6740ac3637bda5694d30814a9f525f694b4e8db9
qweqweq1222 <quankyyam@gmail.com>
2023-05-16 08:37:46 +0300

    Update dirs.sh

--- dirs.sh
+++ dirs.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-mkdir qr
-mkdir dm 
-mkdir -p results/plots
-mkdir -p results/images
-mkdir -p results/images/wrong  
-mkdir -p results/images/all  
-mkdir -p test_dataset/images
-mkdir -p test_dataset/labels
-mkdir -p results/images/wrong/qr
-mkdir -p results/images/wrong/dm
-mkdir -p results/stats_txt
+mkdir "qr";
+mkdir "dm"; 
+mkdir -p "results/plots";
+mkdir -p "results/images";
+mkdir -p "results/images/wrong";
+mkdir -p "results/images/all";
+mkdir -p "test_dataset/images";
+mkdir -p "test_dataset/labels";
+mkdir -p "results/images/wrong/qr";
+mkdir -p "results/images/wrong/dm";
+mkdir -p "results/stats_txt";
 unzip qr_codes.zip -d qr/
 unzip dm_codes.zip -d dm/
 cp qr/train/images/* test_dataset/images
