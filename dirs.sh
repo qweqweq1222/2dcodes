ae025015c0aaecf47ab30dd1633184a5fc29da2a
qweqweq1222 <quankyyam@gmail.com>
2023-05-16 08:46:44 +0300

    Update dirs.sh

--- dirs.sh
+++ dirs.sh
@@ -10,9 +10,9 @@ mkdir -p "test_dataset/labels";
 mkdir -p "results/images/wrong/qr";
 mkdir -p "results/images/wrong/dm";
 mkdir -p "results/stats_txt";
-unzip qr_codes.zip -d qr/
-unzip dm_codes.zip -d dm/
-cp qr/train/images/* test_dataset/images
-cp qr/train/labels/* test_dataset/labels
-cp dm/train/images/* test_dataset/images
-cp dm/train/labels/* test_dataset/labels
+unzip qr_codes.zip -d "qr";
+unzip dm_codes.zip -d "dm";
+cp qr/train/images/* "test_dataset/images";
+cp qr/train/labels/* "test_dataset/labels";
+cp dm/train/images/* "test_dataset/images";
+cp dm/train/labels/* "test_dataset/labels";
